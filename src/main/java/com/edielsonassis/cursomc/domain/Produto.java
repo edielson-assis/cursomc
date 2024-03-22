91bff086393b992933dfcb46a3c39ceeaf515305
Edielson Assis <edielsonnice@hotmail.com>
2024-03-22 12:10:58 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/edielsonassis/cursomc/domain/Produto.java
+++ src/main/java/com/edielsonassis/cursomc/domain/Produto.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,13 +21,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Categoria implements Serializable {
-
+public class Produto implements Serializable {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
+    private Double preco;
 
-    @ManyToMany(mappedBy = "categorias")
-    private final List<Produto> produtos = new ArrayList<>();
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(name = "produto_categoria", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private final List<Categoria> categorias = new ArrayList<>();
 }
