266e66e92fb5a38b8f5703143a46dfbb247888c1
Edielson Assis <edielsonnice@hotmail.com>
2024-03-21 23:44:43 -0300

    Testando o REST

--- src/main/java/com/edielsonassis/cursomc/CursomcApplication.java
+++ src/main/java/com/edielsonassis/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.edielsonassis.cursomc.cursomc;
+package com.edielsonassis.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
